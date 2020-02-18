6f4210ff8c31a657609b099a0699686b4b1ebc77
William Glass <wglass149@googlemail.com>
2020-02-18 15:50:29 +0000

    ci com test

--- tests/calc_angles_test.py
+++ tests/calc_angles_test.py
@@ -1,15 +1,27 @@
 from orientation.setup_system import get_universe
-from orientation.calc_angles import get_principal_axes
+from orientation.calc_angles import get_com, get_principal_axes
 
 import MDAnalysis as mda
 import numpy as np
 import os
 
 sel = "name CA and resid 1:123"
+
 gro_file = os.path.join(os.getcwd(), "data", "b3_syst_protein_only.gro")
+
 traj_file = os.path.join(os.getcwd(), "data", "b3_frm_human_b1_r0_400ns_noPBCWhole_noJump_Center_SKIP10.xtc")
 
 uni = get_universe(gro_file, traj_file)
+
+
+def test_get_com():
+
+    ref_com = uni.select_atoms(sel).center_of_mass()
+
+    com = get_com(uni, sel)
+
+    assert (ref_com==com).all()
+
 
 def test_pa():
 
