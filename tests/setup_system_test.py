ba14f8eed2d449b3765c5150d040aa9250ca47cb
William Glass <wglass149@googlemail.com>
2020-06-08 15:41:53 +0100

    reformat setup_system_test.py

--- tests/setup_system_test.py
+++ tests/setup_system_test.py
@@ -1,29 +1,33 @@
-from orientation.setup_system import get_universe, read_stride
-
-import MDAnalysis as mda
+import filecmp
 import os
 import pickle
-import filecmp
+
+import MDAnalysis as mda
+from orientation.setup_system import get_universe, read_stride
 
 gro_file = os.path.join(os.getcwd(), "data", "b3_syst_protein_only.gro")
-traj_file = os.path.join(os.getcwd(), "data", "b3_frm_human_b1_r0_400ns_noPBCWhole_noJump_Center_SKIP10.xtc")
+traj_file = os.path.join(
+    os.getcwd(), "data", "b3_frm_human_b1_r0_400ns_noPBCWhole_noJump_Center_SKIP10.xtc"
+)
 
 stride_file = os.path.join(os.getcwd(), "data", "beta3_stride_file.txt")
 sec_struc_file = os.path.join(os.getcwd(), "data", "sec_struc.txt")
 protein_length = 166
+
 
 def test_get_universe():
 
     uni = get_universe(gro_file, traj_file)
 
     assert isinstance(uni, mda.Universe)
-    
+
 
 def test_get_universe_notraj():
 
     uni = get_universe(gro_file)
-    
+
     assert isinstance(uni, mda.Universe)
+
 
 def test_read_stride():
 
@@ -34,8 +38,7 @@ def test_read_stride():
     # compare to ref
     ref_dict_path = os.path.join(os.getcwd(), "data", "ref_stride_output_dict.pkl")
 
-    with open(ref_dict_path, 'rb') as file:
+    with open(ref_dict_path, "rb") as file:
         ref_dict = pickle.load(file)
 
     assert current_dict == ref_dict
-
