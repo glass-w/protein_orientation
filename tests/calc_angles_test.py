25e363a5292f81d5bf9ea2017107d5beaf890252
William Glass <wglass149@googlemail.com>
2020-06-08 15:40:41 +0100

    reformat visuals.py and calc_angles_test.py

--- tests/calc_angles_test.py
+++ tests/calc_angles_test.py
@@ -1,15 +1,23 @@
-from orientation.setup_system import get_universe
-from orientation.calc_angles import get_com, get_principal_axes, dir_cosine, make_direction_cosine_matrix
+import os
+
+import numpy as np
 
 import MDAnalysis as mda
-import numpy as np
-import os
+from orientation.calc_angles import (
+    dir_cosine,
+    get_com,
+    get_principal_axes,
+    make_direction_cosine_matrix,
+)
+from orientation.setup_system import get_universe
 
 sel = "name CA and resid 1:123"
 
 gro_file = os.path.join(os.getcwd(), "data", "b3_syst_protein_only.gro")
 
-traj_file = os.path.join(os.getcwd(), "data", "b3_frm_human_b1_r0_400ns_noPBCWhole_noJump_Center_SKIP10.xtc")
+traj_file = os.path.join(
+    os.getcwd(), "data", "b3_frm_human_b1_r0_400ns_noPBCWhole_noJump_Center_SKIP10.xtc"
+)
 
 uni = get_universe(gro_file, traj_file)
 
@@ -31,37 +39,38 @@ def test_pa():
 
     sel = "name CA and resid 1:123"
     uni = get_universe(gro_file, traj_file)
-    
+
     CA = uni.select_atoms(sel)
     I = CA.moment_of_inertia()
 
     U = get_principal_axes(uni, sel)
 
     Lambda = U.T.dot(I.dot(U))
-    
+
     assert np.allclose(Lambda - np.diag(np.diagonal(Lambda)), 0)
+
 
 def test_dir_cosine():
 
-    v1 = np.array([0,0,1])
-    v2 = np.array([0,1,0])
+    v1 = np.array([0, 0, 1])
+    v2 = np.array([0, 1, 0])
 
-    # gives dot prod 
+    # gives dot prod
     dc = dir_cosine(v1, v2)
-
 
     assert dc == 0.0
 
+
 def test_make_direction_cosine_matrix():
 
-    ref_array = np.array([[1,0,0], [0,1,0], [0,0,1]])
+    ref_array = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 
-    axes = np.array([[1,0,0], [0,1,0], [0,0,1]])
+    axes = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 
     CM = make_direction_cosine_matrix(ref_array, axes)
 
     # convert matrix to array for assertion
-    #CM = np.asarray(CM)
+    # CM = np.asarray(CM)
 
     # assert shape and that CM is an identity matrix (for these orthogonal vectors)
     assert (CM.shape[0] == CM.shape[1]) and (CM == np.eye(CM.shape[0])).all()
